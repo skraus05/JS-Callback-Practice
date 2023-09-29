57abf11c45e3eb6afcdecb5be25dd53e371e591c
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-09-28 17:28:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Stays in index.js
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
@@ -7,68 +8,10 @@ move(newImage('assets/tree.png')).withArrowKeys(200, 450);
 let direction = null;
 let x = 100;
 let y = 250;
-
-function moveCharacter(){
-if(direction === 'west'){
-    x = x - 1;
-}
-if(direction === 'north'){
-    y = y + 1;
-}
-if(direction === 'east'){
-    x = x + 1;
-}
-if(direction === 'south'){
-    y = y - 1;
-}
-}
-character.style.left = x + 'px';
-character.style.bottom = y + 'px';
+//Up to here
 
 
-move(character).to(100, 250);
-
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-    if (e.key === 'ArrowLeft') {
-        direction = 'west';
-    }
-    if (e.key === 'ArrowUp') {
-        direction = 'north';
-    }
-    if (e.key === 'ArrowRight') {
-        direction = 'east';
-    }
-    if (e.key === 'ArrowDown') {
-        direction = 'south';
-    }
-    callback(direction);
-})
-
-document.addEventListener('keyup', function(e) {
-    direction = null;
-    callback(direction);
-})
-
-function move(element) {
-    element.style.position = 'fixed'
-
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    }
-
-    function moveWithArrowKeys(left, bottom){
-       
-    }
-
-    return {
-        to: moveToCoordinates,
-        withArrowKeys: moveWithArrowKeys
-    }        
-}
-
-function handleDirectionChange(direction){
+function handleDirectionChange(direction){ //Stays in index.js
 if(direction === null){
     character.src = 'assets/green-character/static.gif'
 }
@@ -99,7 +42,7 @@ function moveWithArrowKeys(left, bottom, callback){
 
 
 
-
+//Stays in index,js
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
